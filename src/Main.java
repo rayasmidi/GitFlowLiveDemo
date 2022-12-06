34afbd3833fe00bb877fb1a092738f10924b7173
Raya Smidi <rayasmidi@MacBook-Pro-6.local>
2022-12-05 22:49:41 -0500

    Added calculator method

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        System.out.println("2+2=" + addTwoPlusTwo());
+    }
+
+    public static int addTwoPlusTwo() {
+        return 2 + 2;
     }
 }
